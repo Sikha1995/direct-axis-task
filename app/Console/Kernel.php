897c6a2c1ae857b9b9a555c5c3b27b6ea1951c10
Sikha1995 <142133876+Sikha1995@users.noreply.github.com>
2023-08-13 12:53:30 +0400

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Jobs\SendEmailJob; 
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +14,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $details['email'] = 'sikhaouvady@gmail.com';
+
+        $schedule->job(new SendEmailJob($details))->hourly();
     }
 
     /**
